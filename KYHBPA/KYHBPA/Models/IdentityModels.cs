278fbe26fc842a4b8fd6b4f8fd9ad2f0e8d3b933
CoreyCooley <Corey.a.Cooley@gmail.com>
2017-09-13 17:42:18 -0400

    Add Member Model, Controller, and Views

--- KYHBPA/KYHBPA/Models/IdentityModels.cs
+++ KYHBPA/KYHBPA/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace KYHBPA.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Member> Members { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
