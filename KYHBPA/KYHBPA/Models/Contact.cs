774d6e8b9a1bef1d6512e058b02b851d391c6694
Andrew W Walz <awwalz01@louisville.edu>
2017-09-18 15:47:04 -0400

    Added contact model

---
+++ KYHBPA/KYHBPA/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace KYHBPA.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+    }
+}
