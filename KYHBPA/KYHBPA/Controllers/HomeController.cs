7c6b610734e5eb37370c054ac2d29690bfb63339
Corey Cooley <corey.a.cooley@gmail.com>
2017-09-20 15:51:31 -0400

    Update Document Database

--- KYHBPA/KYHBPA/Controllers/HomeController.cs
+++ KYHBPA/KYHBPA/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KYHBPA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace KYHBPA.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +29,52 @@ namespace KYHBPA.Controllers
 
             return View();
         }
+        public ActionResult DocumentManagement()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult UploadDocument(int memberId, HttpPostedFileBase file)
+        {
+
+            byte[] uploadedFile = new byte[file.InputStream.Length];
+            file.InputStream.Read(uploadedFile, 0, uploadedFile.Length);
+
+            var member = db.Members.FirstOrDefault(doc => doc.Id == memberId);
+
+            if (member != null)
+            {
+                var documentModel = new Document
+                {
+                    MemberId = member.Id,
+                    UploadedBy = HttpContext.User.Identity.Name,
+                    ContentLength = file.ContentLength,
+                    ContentType = file.ContentType,
+                    FileName = file.FileName,
+                    FileBytes = uploadedFile
+                };
+
+                db.Documents.Add(documentModel);
+                db.SaveChanges();
+            }
+
+            return View("DocumentManagement");
+        }
+        /*
+        public ActionResult GetDocument(int studentId)
+        {
+            var allDocumentsForStudent = _db.Documents.Where(d => d.StudentId == studentId);
+
+            var oneDocumentFromStudent = allDocumentsForStudent.FirstOrDefault();
+
+            if (oneDocumentFromStudent != null)
+            {
+                return File(oneDocumentFromStudent.FileBytes, "application/octet-stream", oneDocumentFromStudent.FileName);
+            }
+            return RedirectToAction("DocumentManagement");
+        }
+        */
     }
 }
