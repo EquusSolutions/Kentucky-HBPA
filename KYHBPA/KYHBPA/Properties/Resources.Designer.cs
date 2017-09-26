3ab1097f1d677f6d8951b84d9d33caeead007965
steeltownriot <steeltownriot@gmail.com>
2017-09-26 16:01:17 -0400

    Added logo and made minor html changes to _Layout and home page

--- KYHBPA/KYHBPA/Properties/Resources.Designer.cs
+++ KYHBPA/KYHBPA/Properties/Resources.Designer.cs
@@ -109,5 +109,15 @@ namespace KYHBPA.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap kyhbpa_logo {
+            get {
+                object obj = ResourceManager.GetObject("kyhbpa_logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
