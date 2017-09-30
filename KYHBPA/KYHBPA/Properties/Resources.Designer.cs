667b52732247d18306c424397902817085f8b6fa
steeltownriot <steeltownriot@gmail.com>
2017-09-30 13:39:11 -0400

    Added twitter feed

--- KYHBPA/KYHBPA/Properties/Resources.Designer.cs
+++ KYHBPA/KYHBPA/Properties/Resources.Designer.cs
@@ -229,5 +229,15 @@ namespace KYHBPA.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap Twitter {
+            get {
+                object obj = ResourceManager.GetObject("Twitter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
