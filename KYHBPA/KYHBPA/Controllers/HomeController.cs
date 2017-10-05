21e79538587bb79f8ef9d305ad5b31f24fd7954e
Andrew W Walz <awwalz01@louisville.edu>
2017-10-04 21:10:53 -0400

    attempt at contact email.  recaptcha added in

--- KYHBPA/KYHBPA/Controllers/HomeController.cs
+++ KYHBPA/KYHBPA/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using KYHBPA.Models;
+using reCAPTCHA.MVC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,6 +32,74 @@ namespace KYHBPA.Controllers
 
             return View();
         }
+
+    
+        [HttpPost]
+        [CaptchaValidator(
+           PrivateKey = "6LetNjMUAAAAAD59qvyXAymsz6rL7gaJeha99xwb",
+           ErrorMessage = "Invalid input captcha.",
+           RequiredMessage = "The captcha field is required.")]
+        public ActionResult Contact(Contact viewModel)
+        {
+
+            var IsDebug = false;
+
+#if DEBUG
+            IsDebug = true;
+#endif
+
+            var toEmail = "equus.web.solutions@gmail.com";
+            var toEmailPassword = "!23Password";
+
+            //Debug SMTP Settings
+            var debugSmtpClient = new SmtpClient();
+            debugSmtpClient.Host = "smtp.gmail.com";
+            debugSmtpClient.Port = 587;
+            debugSmtpClient.EnableSsl = true;
+            //Setup credentials to login to our sender email address("UserName", "Password")
+            NetworkCredential credentials = new NetworkCredential(toEmail, toEmailPassword);
+            debugSmtpClient.UseDefaultCredentials = false;
+            debugSmtpClient.Credentials = credentials;
+
+            //Release SMTP Settings
+            var releaseSmtpClient = new SmtpClient();
+            releaseSmtpClient.EnableSsl = false;
+            releaseSmtpClient.Host = "relay-hosting.secureserver.net";
+            releaseSmtpClient.Port = 25;
+
+
+            if (ModelState.IsValid || (ModelState.IsValid == false && IsDebug))
+            {
+                string Body = viewModel.Note;
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add(toEmail);
+                mail.From = new MailAddress(viewModel.Email);
+                mail.Subject = "Contact Form Submission";
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+#if DEBUG
+                smtp = debugSmtpClient;
+#else
+                smtp = releaseSmtpClient;
+#endif
+                try
+                {
+                    smtp.Send(mail);
+                }
+                catch (SmtpException)
+                {
+                    return View("EmailError");
+                }
+                return View("Index");
+            }
+            else
+            {
+                return View("Index", viewModel);
+            }
+        }
+
         public ActionResult DocumentManagement()
         {
             return View();
